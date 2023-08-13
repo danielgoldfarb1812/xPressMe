06b323a10019bafe06c194854b86388e11d7f57a
daniel <danielgold5618@gmail.com>
2023-08-13 16:54:21 +0300

    added splash screen

---
+++ app/src/main/java/com/example/xpressme/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.xpressme;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        setContentView(R.layout.activity_splash);
+
+        int DELAY_TIME = 2000;
+        new Handler().postDelayed(() -> {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+        }, DELAY_TIME);
+    }
+}
